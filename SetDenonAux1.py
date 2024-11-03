1bef953566c4b27026a1d2d44294172a62e5e075
c.saller <christoph.saller@gmail.com>
2024-11-03 13:12:20 +0100

    Restart trx-rx if soundcard is not active but should be

--- SetDenonAux1.py
+++ SetDenonAux1.py
@@ -4,7 +4,9 @@ import os
 import time
 import subprocess
 import requests
+import sys
 from time import sleep
+from enum import Enum
 
 PLAYING = 1
 OFFLINE = 2
@@ -24,38 +26,76 @@ def isRunningHTTP():
 
 def isRunningLocal():
 	if debugOutput :
+		#print  (os.popen('cat /proc/asound/card*/pcm*/sub*/status | grep RUNNING').read().split()   )
+		print (os.popen('sudo moodeutl -d -gv rxactive').read())
+    	#will return True if "Running" is found in any sound card.
+	if "1" in os.popen('sudo moodeutl -d -gv rxactive').read():
+		#print ("rx active")
+		return True
+	else:
+		#print ("rx inactive")
+		return False
+
+def isRunningSoundcard():
+	if debugOutput :
 		print  (os.popen('cat /proc/asound/card*/pcm*/sub*/status | grep RUNNING').read().split()   )
     	#will return True if "Running" is found in any sound card.
 	return "RUNNING" in os.popen('cat /proc/asound/card*/pcm*/sub*/status | grep RUNNING').read().split()
 	
 
-#When running in multiRoom mode, the soundcard will always return "RUNNING" as the connection is kept open
-#As a workaround, ask the server if actually sending music
+
 def isRunning(): 
 	if isRunningLocal():
+		#print("local on")
 		if multiRoomActive:
 			if isRunningHTTP():
+				#print("HTTP true")					
 				return True
 			else:
+				#print("HTTP false")
 				return False
 		else:
+				#print("multiroom off ")
 				return True
 	else:
+		#print("local off")
 		return False
+
 	
 while True:
-	status = isRunning()
-	if status == True and currentState == OFFLINE :         
-		subprocess.call(['expect', '/home/moode/SetDenonAux1.sh']) 
-		currentState = PLAYING 
-		print("Denon set to aux1")
+	try:
+		status = isRunning()
+	
+		if status == True and currentState == OFFLINE :  
+			try:  
+				subprocess.call(['expect', '/home/moode/SetDenonAux1.sh'])
+			except:
+				print("telnet problem") 
+			#workaround for HDMI issue: restart RX client
+			if isRunningSoundcard() == False:
+				print("restarting trx-rx")
+				os.popen('sudo killall trx-rx').read()
+				sleep(1)
+				try:
+					os.popen('sudo trx-rx -d default:vc4hdmi0 -h 239.0.0.1 -p 1350 -m 128 -j 64 -f 960 -R 45 -D /tmp/trx-rxpid ')
+				except:
+					print("trx-rx error")
+				sleep(2)
+			currentState = PLAYING 
+			print("Denon set to aux1")
 		
-	elif status == False and currentState == PLAYING : 
-		currentState = OFFLINE 
-		print("stopped, dont change Denon CH")    
+		elif status == False and currentState == PLAYING : 
+			currentState = OFFLINE 
+			print("stopped, dont change Denon CH")    
                                  
-	else:
-		if debugOutput :
-			print("No State Change...")
-    
-	sleep(0.5)
+		else:
+			pass
+			if debugOutput :
+				print("No State Change...")
+		sleep(0.5)
+	except KeyboardInterrupt:
+		sys.exit(0)
+	except:
+		status = False
+		print("httping not possible")
+	
--- SetDenonAux1.sh
+++ SetDenonAux1.sh
@@ -1,10 +1,21 @@
 #!/usr/bin/expect
-spawn telnet 192.168.178.21 23
+set IPaddress 192.168.178.21
+
+spawn ping -c 3 $IPaddress 
+expect  {
+  " 0%"   {puts "$IPaddress  Is Up"}
+  " 100%" {puts "$IPaddress  Is Down"}
+}
+
+spawn telnet $IPaddress  23
   sleep 1
   send PW?\r
   expect {
   	PWSTANDBY {send PWON\r; sleep 8}
-	  PWON	    {sleep 1}
+	  PWON	  {sleep 1}
   }
-  send SIAUX1\r:
-  send ^]
+  send SIAUX1\r
+  sleep 1
+  send SIAUX1\r
+  # Send special ^] to telnet so we can tell telnet to quit.
+  send ^] 
